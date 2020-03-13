3f6a838b4de27ea72035eccea5d6e0a76c98456b
Isaac Smith <isaacsmith215@aol.com>
2020-03-13 23:18:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli < 1
+  if katz_deli.size < 1
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli.first}."
