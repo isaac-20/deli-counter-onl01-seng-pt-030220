011bda52ec614533d06e13a70865c12cdc24a4e4
Isaac Smith <isaacsmith215@aol.com>
2020-03-13 22:56:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  line = []
+  if katz_deli.size < 1 
+    puts "The line is currently empty."
+  else
+      number = 1
+    katz_deli.each do |customer|
+      line.push("#{number}. #{customer}")
+      number += 1
+    end
+    puts "The line is currently: #{line.join(" ")}"
+
+  end
+end
+def take_a_number(name,line1)
+  line1.push(name)
+  puts "#{line1[line1.size-1]}"
+end
