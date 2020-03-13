1ea096c3ef1d215a3ba277d9dc2908500406d5f3
Isaac Smith <isaacsmith215@aol.com>
2020-03-13 23:16:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   if katz_deli.size < 1 
     puts "The line is currently empty."
   else
-      number = 1
+    number = 1
     katz_deli.each do |customer|
       line.push("#{number}. #{customer}")
       number += 1
