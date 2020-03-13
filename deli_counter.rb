9d674f692693adc25c5ed6b3bb2f7000782d567b
Isaac Smith <isaacsmith215@aol.com>
2020-03-13 23:18:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,16 @@ def line(katz_deli)
 
   end
 end
-def take_a_number(name,line1)
-  line1.push(name)
-  puts "#{line1[line1.size-1]}"
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli < 1
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
 end
